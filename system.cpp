7872d2f4c5ee2e392abe4dc2c48eb3e48d959d11
Shahid Ali <bajeershahid125@gmail.com>
2025-10-29 10:46:11 -0700

    first commit

---
+++ system.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int age ;
+    cout << "Enter your age:";
+    cin >> age;
+    cout <<"you are" << age << "years old." <<endl;
+    return 0;
+    
+}
